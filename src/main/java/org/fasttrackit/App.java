f69b6de8d7f1a17aeab8cb5e7040af422627d0eb
TTobi96 <tobiastamas96@yahoo.com>
2020-02-24 21:23:17 +0200

    studied aggregation

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -15,6 +15,27 @@ public class App
         carReference.maxSpeed = 260;
         carReference.mileage = 6.2;
 
+//        carReference.engine = new Engine();
+//        carReference.engine.manufacturer = "VW";
+//        carReference.engine.capacity = 2000;
+
+        Engine engine1 = new Engine();
+        engine1.manufacturer = "VW";
+
+        engine1.capacity = 2000;
+
+        carReference.engine = engine1;
+
+        System.out.println("Engine details:");
+        System.out.println(engine1.capacity);
+        System.out.println(carReference.engine.capacity);
+
+        engine1.capacity=2100;
+
+        System.out.println("Updated engine details:");
+        System.out.println(engine1.capacity);
+        System.out.println(carReference.engine.capacity);
+
         //concatenare ...sout ( rovidites)
         System.out.println("Properties of " + carReference.name);
         System.out.println("Color: " + carReference.color);
@@ -35,7 +56,7 @@ public class App
         System.out.println("Damaged is " +car2.damaged);
         System.out.println("Color is " + car2.color);
 
-        
+
 
     }
 }
